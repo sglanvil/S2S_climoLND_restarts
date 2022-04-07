e235c976f0f7a9f1463d7b77b81e44cda0ca1e92
Jim Edwards <jedwards@ucar.edu>
2022-04-07 14:03:31 -0600

    fixed climo making

--- makeClimoLNDforcing.sh
+++ makeClimoLNDforcing.sh
@@ -5,10 +5,21 @@ outDir='/glade/scratch/ssfcst/climoLND_forcingFiles/'
 
 mkdir -p ${outDir}
 
-for imonth in {01..12}; do
-	echo ${imonth}
-	ncra -O ${inDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Solar.*-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Solar.CLIMO-${imonth}.nc
-	ncra -O ${inDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.TPQWL.*-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.TPQWL.CLIMO-${imonth}.nc
-	ncra -O ${inDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Precip.*-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Precip.CLIMO-${imonth}.nc
+#for imonth in {01..12}; do
+#	echo ${imonth}
+#	ncra -O ${inDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Solar.*-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Solar.CLIMO-${imonth}.nc
+#	ncra -O ${inDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.TPQWL.*-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.TPQWL.CLIMO-${imonth}.nc
+#	ncra -O ${inDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Precip.*-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Precip.CLIMO-${imonth}.nc
+#done
+
+# the above stuff was a hella mistake, so redo:
+for ivar in Solar TPQWL Precip; do
+	echo ${ivar}
+	for imonth in {01..12}; do
+		echo ${imonth}
+		ncecat -O ${inDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.${ivar}.????-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.${ivar}.ALL-${imonth}.nc
+		ncwa -O -a record ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.${ivar}.ALL-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.${ivar}.CLIMO-${imonth}.nc
+		ncks  --mk_rec_dmn time -O ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.${ivar}.CLIMO-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.${ivar}.CLIMO-${imonth}.nc 
+	done
 done
 
