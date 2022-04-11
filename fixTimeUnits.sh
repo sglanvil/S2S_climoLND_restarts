c69845dcf1324612b0811f7492473861adb55f50
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-04-11 12:51:35 -0600

    Update fixTimeUnits.sh

--- fixTimeUnits.sh
+++ fixTimeUnits.sh
@@ -12,6 +12,3 @@ for iyear in {1980..2021}; do
                 ncap2 -O -s "time@units=\"days since ${iyear}-${imonth}-01 00:00:00\"" ${climoDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Solar.${iyear}-${imonth}.nc ${climoDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Solar.${iyear}-${imonth}.nc
         done
 done
-
-
-
