69b3a12bc25c938b82230c6efd883e5f5d7874c8
Jim Edwards <jedwards@ucar.edu>
2022-04-11 12:50:20 -0600

    script to fix nc metadata

---
+++ fixTimeUnits.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+climoDir='/glade/scratch/ssfcst/climoLND_forcingFiles/'
+
+for iyear in {1980..2021}; do
+        echo ${iyear}
+        for imonth in {01..12}; do
+                ncap2 -O -s "time@units=\"days since ${iyear}-${imonth}-01 00:00:00\"" ${climoDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.TPQWL.${iyear}-${imonth}.nc ${climoDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.TPQWL.${iyear}-${imonth}.nc
+
+                ncap2 -O -s "time@units=\"days since ${iyear}-${imonth}-01 00:00:00\"" ${climoDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Precip.${iyear}-${imonth}.nc ${climoDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Precip.${iyear}-${imonth}.nc
+
+                ncap2 -O -s "time@units=\"days since ${iyear}-${imonth}-01 00:00:00\"" ${climoDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Solar.${iyear}-${imonth}.nc ${climoDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Solar.${iyear}-${imonth}.nc
+        done
+done
+
+
+
