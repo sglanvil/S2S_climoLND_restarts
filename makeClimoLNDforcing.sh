53841ba6229c4316f41c849f9e3ec6edc0e10690
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-04-21 14:03:58 -0600

    Update makeClimoLNDforcing.sh

--- makeClimoLNDforcing.sh
+++ makeClimoLNDforcing.sh
@@ -5,14 +5,6 @@ outDir='/glade/scratch/ssfcst/climoLND_forcingFiles/'
 
 mkdir -p ${outDir}
 
-#for imonth in {01..12}; do
-#	echo ${imonth}
-#	ncra -O ${inDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Solar.*-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Solar.CLIMO-${imonth}.nc
-#	ncra -O ${inDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.TPQWL.*-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.TPQWL.CLIMO-${imonth}.nc
-#	ncra -O ${inDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Precip.*-${imonth}.nc ${outDir}clmforc.NCEPCFSv2.c2019.0.2d.576x1152.Precip.CLIMO-${imonth}.nc
-#done
-
-# the above stuff was a hella mistake, so redo:
 for ivar in Solar TPQWL Precip; do
 	echo ${ivar}
 	for imonth in {01..12}; do
